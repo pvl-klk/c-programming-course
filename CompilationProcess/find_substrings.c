41d5b61216bf6897ce485bed1ded70dd68ccf0cf
Pavel Kuliaka <pavel.kulyaka@yandex.ru>
2025-10-02 01:31:24 +0300

    Implemented counting of substring entries

---
+++ CompilationProcess/find_substrings.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    const char* string = "1301020301010203";
+    const char* substring = "01";
+
+    const unsigned int stringLength = strlen(string);
+    const unsigned int substringLength = strlen(substring);
+
+    int counter = 0;
+    for (int offset = 0; offset < stringLength - substringLength + 1; ++offset) {
+        int flag = 1;
+        for (int index = 0; index < substringLength; ++index) {
+            if (string[index + offset] != substring[index]) {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag == 1) {
+            ++counter;
+        }
+    }
+
+    printf("The number of entries is %d\n", counter);
+
+    return 0;
+}
+
