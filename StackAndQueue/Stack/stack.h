555a2d19b0c4d52b5a60ebceb4a3235912e590ef
Pavel Kuliaka <pavel.kulyaka@yandex.ru>
2025-10-19 13:52:25 +0300

    Added comments to stack.h

--- StackAndQueue/Stack/stack.h
+++ StackAndQueue/Stack/stack.h
@@ -1,14 +1,20 @@
 #pragma once
 
+// The StackStruct structure with the value and previous fields
+// Defined as Stack
 typedef struct StackStruct {
     void * value;
     struct StackStruct *previous;
 } Stack;
 
+// Create a new stack
 Stack* stackNew();
 
+// Add a new element to the top of the stack
 void stackPush(Stack**, void*);
 
+// Remove an element from the top of the stack
 void stackPop(Stack**);
 
+// Free the stack
 void stackFree(Stack**);
