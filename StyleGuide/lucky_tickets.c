49558c0cf086c04044e4e121f2069757d80418db
Pavel Kuliaka <pavel.kulyaka@yandex.ru>
2025-12-14 20:56:12 +0300

    Fixed small problems with types

--- StyleGuide/lucky_tickets.c
+++ StyleGuide/lucky_tickets.c
@@ -4,7 +4,7 @@ unsigned int countLuckyTickets(void)
     for (int digit_1 = 0; digit_1 < 10; ++digit_1) {
         for (int digit_2 = 0; digit_2 < 10; ++digit_2) {
             for (int digit_3 = 0; digit_3 < 10; ++digit_3) {
-                unsigned short amount = digit_1 + digit_2 + digit_3;
+                unsigned int amount = digit_1 + digit_2 + digit_3;
                 variants[amount] += 1;
             }
         }
