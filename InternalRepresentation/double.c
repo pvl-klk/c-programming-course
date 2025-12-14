0b4b902145e10fece47f48a0f2ab749e6687206e
Pavel Kuliaka <pavel.kulyaka@yandex.ru>
2025-12-14 16:20:44 +0300

    Formatted file

--- InternalRepresentation/double.c
+++ InternalRepresentation/double.c
@@ -1,4 +1,4 @@
-/* 
+/*
     В C для типа double используется стандарт IEEE 754
     IEEE 754 - стандарт размещения вещественных чисел в памяти
 
@@ -17,25 +17,26 @@ typedef union {
     unsigned long long bits;
 } DoubleUnion;
 
-int main(void) {
+int main(void)
+{
     DoubleUnion doubleUnion;
     printf("Enter a number: ");
     if (scanf("%lf", &doubleUnion.value) != 1) {
         return 1;
     }
-    
+
     unsigned long long bits = doubleUnion.bits;
-    
+
     // Берем 63 бит
     char sign = (bits >> 63) ? '-' : '+';
     // Берем с 62 по 52 биты
     unsigned long long exponent = (bits >> 52) & 0x7FFUL;
     // Берем последние 52 бита
     unsigned long long mantissa = bits & 0xFFFFFFFFFFFFFUL;
-    
-    double mantissa_value = 1.0 + (double) mantissa / (1ULL << 52);
-    int real_exponent = (int) exponent - 1023;
-    
+
+    double mantissa_value = 1.0 + (double)mantissa / (1ULL << 52);
+    int real_exponent = (int)exponent - 1023;
+
     printf("Result: %c%.20g*2^%d\n", sign, mantissa_value, real_exponent);
 
     return 0;
